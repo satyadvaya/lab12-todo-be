38fdf1f63fe70fcd31a88bb858412cf7a46f4b4f
satyadvaya <satyadvaya@gmail.com>
2021-08-23 18:08:55 -0700

    working test for GET route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -28,35 +28,67 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    // test('returns animals', async() => {
+
+    //   const expectation = [
+    //     {
+    //       'id': 1,
+    //       'name': 'bessie',
+    //       'cool_factor': 3,
+    //       'owner_id': 1
+    //     },
+    //     {
+    //       'id': 2,
+    //       'name': 'jumpy',
+    //       'cool_factor': 4,
+    //       'owner_id': 1
+    //     },
+    //     {
+    //       'id': 3,
+    //       'name': 'spot',
+    //       'cool_factor': 10,
+    //       'owner_id': 1
+    //     }
+    //   ];
+
+    //   const data = await fakeRequest(app)
+    //     .get('/animals')
+    //     .expect('Content-Type', /json/)
+    //     .expect(200);
+
+    //   expect(data.body).toEqual(expectation);
+    // });
+
+    test('returns todos', async() => {
 
       const expectation = [
         {
-          'id': 1,
-          'name': 'bessie',
-          'cool_factor': 3,
-          'owner_id': 1
+          id: 1,
+          todo: 'do laundry',
+          completed: true,
+          user_id: 1
         },
         {
-          'id': 2,
-          'name': 'jumpy',
-          'cool_factor': 4,
-          'owner_id': 1
+          id: 2,
+          todo: 'learn Sanskrit',
+          completed: false,
+          user_id: 1
         },
         {
-          'id': 3,
-          'name': 'spot',
-          'cool_factor': 10,
-          'owner_id': 1
+          id: 3,
+          todo: 'rid world of violence',
+          completed: false,
+          user_id: 1
         }
       ];
 
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/todos')
         .expect('Content-Type', /json/)
         .expect(200);
 
       expect(data.body).toEqual(expectation);
     });
+
   });
 });
---
+++ data/todos.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    id: 1,
+    todo: 'do laundry',
+    completed: true,
+    user_id: 2
+  },
+  {
+    id: 2,
+    todo: 'learn Sanskrit',
+    completed: false,
+    user_id: 2
+  },
+  {
+    id: 3,
+    todo: 'rid world of violence',
+    completed: false,
+    user_id: 2
+  }
+];
